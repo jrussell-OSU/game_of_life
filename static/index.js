5d300773580165b162a728085b9e4d49e5f687e0
Jacob Russell <jacob.r.russell@gmail.com>
2022-12-15 17:25:08 -0500

    added work history to index

--- static/index.js
+++ static/index.js
@@ -1,12 +1,14 @@
 
-FADE_TIME = 1000;
+const FADE_TIME = 1000;
 
-function view_about_me() {
+
+//These functions swap which div we are viewing on the main body of the page
+function view_home() {
+    $("#about_me_frame").hide();
+    $("#home_frame").fadeIn(FADE_TIME);
+
+}function view_about_me() {
     $("#home_frame").hide();
     $("#about_me_frame").fadeIn(FADE_TIME);
 }
 
-function view_home() {
-    $("#about_me_frame").hide();
-    $("#home_frame").fadeIn(FADE_TIME);
-}
