d04c65030abdb762003568d7c66ecb60d70cbd56
jrussell-OSU <jacob.r.russell@gmail.com>
2023-05-11 19:32:38 -0400

    fix: fixed issue where index was looking for wrong css file

--- app.js
+++ app.js
@@ -21,42 +21,6 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-//Fetches first paragraph of Conway's Game of Life wikipedia page, to be later displayed on /game_of_life to the user
-async function get_wiki_text() {
-  try {
-    const query_params = {
-        "action": "query",
-        "format": "json",
-        "prop": "extracts",
-        "titles": "Conway's_Game_of_Life",
-        "formatversion": "2",
-        "exsentences": "5",
-        "explaintext": true,
-        "exintro": true
-      }
-
-    const params = new url.URLSearchParams(query_params);
-    const response = await axios.get(`https://en.wikipedia.org/w/api.php?${params}`);
-    let result = response.data.query.pages[0].extract;
-    result = JSON.stringify(result);
-    //result = result.replace(/<[^>]*>/g);
-    const context = {
-      wiki_text: result
-    }
-    return context;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-app.get('/game_of_life', async function (req, res) {
-  const context = await get_wiki_text();  //include the "more info" text we got from wikipedia Conway's Game of Life article
-  res.render('game_of_life', {context: context});
-});
-
-
-
-
 //-------------RUN APP----------------
 
 
